60a6e1f69946eb68b63fd6f09d38cba479ce370a
Gillian Tunney <gillian@whoisstudio.com>
2018-09-11 14:50:33 -0400

    more newer junk - kinda confused still

--- source/js/custom/init.js
+++ source/js/custom/init.js
@@ -21,36 +21,6 @@ $( document ).ready(function() {
     });
 });
 
-(function(w){
-	var sw = document.body.clientWidth,
-		sh = document.body.clientHeight;
-
-	$(w).resize(function(){ //Update dimensions on resize
-		sw = document.body.clientWidth;
-		sh = document.body.clientHeight;
-		
-		//updateAds();
-	});
-    
-    
- 
-    
-
-	//Navigation toggle
-	$('.nav-toggle-menu').click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.nav').toggleClass('active');
-	});
-	
-	//Navigation toggle
-	$('.nav-toggle-search').click(function(e) {
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.header .search-form').toggleClass('active');
-	});
-})(this);
-
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
---
+++ source/js/vendor/vendor.js
@@ -0,0 +1,5 @@
+vendor.js
+"use strict";
+
+var m = require('modernizr');
+var R = require('ramda');
